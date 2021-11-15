643439b5f06049975b3e0f65332e0d0e45e1f37d
Christian-Mogensen <U2407192@rts-365.dk>
2021-11-15 18:04:50 +0100

    Add next / tailwind setup

---
+++ newsboxapp/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className='flex justify-center flex-col '>
+      <h1 className='Primary__heading'>hi</h1>
+      <h2 className='Secondary__heading'>hi</h2>
+      <h2 className='Section__heading'>hi</h2>
+      <p className='Sub__title'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis veritatis ipsam, possimus earum est iusto.</p>
+      <p className='Card__title'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis veritatis ipsam, possimus earum est iusto.</p>
+      <p className='Card__copy_small bg-red-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis veritatis ipsam, possimus earum est iusto.</p>
+      <p className='Body__copy_small'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis veritatis ipsam, possimus earum est iusto.</p>
+      <p className='Message__time_stamp'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis veritatis ipsam, possimus earum est iusto.</p>
+    </div>
+  )
+}
