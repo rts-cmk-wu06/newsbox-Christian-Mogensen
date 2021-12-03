b6e17f1c60cec5cb919e0f4b92d901d49ec36e82
Christian-Mogensen <U2407192@rts-365.dk>
2021-12-03 09:42:51 +0100

    Add final changes

--- newsboxapp/pages/archive.js
+++ newsboxapp/pages/archive.js
@@ -154,22 +154,11 @@ const archive = (theme) => {
                   </p>
                 </div>
               </motion.div>
-              <div className="w-full z-10 top-1/2 right-0 absolute h-[calc(100%-2px)] bg-green-400 flex justify-end items-center -translate-y-1/2">
+              <div className="w-full z-10 top-1/2 right-0 absolute h-[calc(100%-2px)] bg-red-400 flex justify-end items-center -translate-y-1/2">
                 <div className="w-12 h-12 flex justify-center items-center m-10">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
-                    />
-                  </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+</svg>
                 </div>
               </div>
             </motion.li>
